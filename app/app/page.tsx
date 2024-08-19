20b416ec0530cdb2ebc15838b3d2c4f29d20252c
alihd1 <ali.m@salla.sa>
2024-08-19 20:19:18 +0300

    add app router page

---
+++ app/app/page.tsx
@@ -0,0 +1,16 @@
+import { JitsuProvider } from "@jitsu/jitsu-react";
+import { jitsuHost, jitsuWriteKey } from "@/env";
+import ToDo from "@/components/ToDo";
+
+export const runtime = "edge";
+
+export default async function Page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  const data = await res.json();
+
+  return (
+    <JitsuProvider options={{ host: jitsuHost, writeKey: jitsuWriteKey }}>
+      <ToDo data={data} />
+    </JitsuProvider>
+  );
+}
