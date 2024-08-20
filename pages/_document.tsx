f2d1f9fbd5c4c1b3ca962b7381ab1cc0d3fdab29
alihd1 <ali.m@salla.sa>
2024-08-20 12:04:09 +0300

    log runtime

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,24 +1,24 @@
+import { jitsuHost, jitsuWriteKey } from "@/env";
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <Script
+        <script
           type="text/javascript"
-          onLoad={() => {
-            if (typeof window !== "undefined" && window.jitsuLoaded) {
-              window.jitsuLoaded = function (jitsu: any) {
-                jitsu.identify("123", { email: "john.doe@gmail.com" });
-                // jitsu.track("customEvent", {customParam: Y})
-              };
-            }
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.jitsuLoaded = function(jitsu) {
+                  jitsu.identify("X", { email: "john.doe@gmail.com" });
+                };
+              `,
           }}
         />
-        <Script
+        <script
           async
-          src="https://st.salla.dev/p.js"
+          src={`${jitsuHost}/p.js`}
+          data-write-key={jitsuWriteKey}
           data-onload="jitsuLoaded"
         />
       </Head>
